3f99e76aa4a08b3093c6771237f613444c2497bc
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2026-06-11 13:16:23 -0300

    feat(i18n): centralize common actions and add new resource loaders

--- src/lib/i18n/config.ts
+++ src/lib/i18n/config.ts
@@ -5,6 +5,11 @@ const config = {
   loaders: [
     {
       locale: "en",
+      key: "common",
+      loader: async () => (await import("./en/common.json")).default,
+    },
+    {
+      locale: "en",
       key: "validation",
       loader: async () => (await import("./en/validation.json")).default,
     },
@@ -20,8 +25,38 @@ const config = {
     },
     {
       locale: "en",
+      key: "subjects",
+      loader: async () => (await import("./en/subjects.json")).default,
+    },
+    {
+      locale: "en",
+      key: "periods",
+      loader: async () => (await import("./en/periods.json")).default,
+    },
+    {
+      locale: "en",
+      key: "commissions",
+      loader: async () => (await import("./en/commissions.json")).default,
+    },
+    {
+      locale: "en",
+      key: "students",
+      loader: async () => (await import("./en/students.json")).default,
+    },
+    {
+      locale: "en",
+      key: "assignments",
+      loader: async () => (await import("./en/assignments.json")).default,
+    },
+    {
+      locale: "en",
       key: "layout",
       loader: async () => (await import("./en/layout.json")).default,
+    },
+    {
+      locale: "es",
+      key: "common",
+      loader: async () => (await import("./es/common.json")).default,
     },
     {
       locale: "es",
@@ -37,6 +72,31 @@ const config = {
       locale: "es",
       key: "faculties",
       loader: async () => (await import("./es/faculties.json")).default,
+    },
+    {
+      locale: "es",
+      key: "subjects",
+      loader: async () => (await import("./es/subjects.json")).default,
+    },
+    {
+      locale: "es",
+      key: "periods",
+      loader: async () => (await import("./es/periods.json")).default,
+    },
+    {
+      locale: "es",
+      key: "commissions",
+      loader: async () => (await import("./es/commissions.json")).default,
+    },
+    {
+      locale: "es",
+      key: "students",
+      loader: async () => (await import("./es/students.json")).default,
+    },
+    {
+      locale: "es",
+      key: "assignments",
+      loader: async () => (await import("./es/assignments.json")).default,
     },
     {
       locale: "es",
